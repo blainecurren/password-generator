7bbd7ba0ad6287e8cbfdea0fd0a68992698f8b2d
Blaine Curren <blaine.curren@gmail.com>
2021-07-26 16:56:04 -0500

    added password length function

--- script.js
+++ script.js
@@ -7,3 +7,22 @@ var passwordLength;
 var uppercaseCheck;
 var numberCheck;
 var specialCheck;
+
+// Password length function
+function determineLength(){
+    passwordLength = prompt("Choose how many characters long you'd like your password to be (between 8-128 characters): ");
+  
+      if (passwordLength<8){
+        alert("Password length must be a number between 8-128 characters");
+        determineLength();
+      }else if (passwordLength>128){
+        alert("Password length must be a number between 8-128 characters");
+        determineLength();
+      }else if (isNaN(passwordLength)){
+        alert("Password length must be a number between 8-128 characters");
+        determineLength();
+      }else{
+      alert("The next three screens will ask you what types of characters you would like to be included in your password.\nIf you choose 'No' for all, your password will only contain lowercase letters.");
+      }
+      return passwordLength;
+  }
