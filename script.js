e5996820cc14a42bb042d598519fbe99e0960349
Blaine Curren <blaine.curren@gmail.com>
2023-01-21 12:15:15 -0600

    added getLength

--- script.js
+++ script.js
@@ -4,6 +4,21 @@ var lowerLetter = "abcdefghijklmnopqrstuvwxyz";
 var specialChars = "!#$%&'()*+,-./:;<=>?@[/]^_`{|}~]";
 var numbers = "0123456789";
 
+function getLength() {
+  passwordLengthPrompt = window.prompt(
+    "How many characters would you like your password to be? (between 8-128 characters):"
+  );
+
+  if (passwordLengthPrompt < 8) {
+    alert("Password length must be between 8-128 characters.");
+    getLength();
+  } else if (passwordLengthPrompt > 128) {
+    alert("Password length must be between 8-128 characters.");
+  } else if (isNaN(passwordLengthPrompt)) {
+    alert("Password length must be a number.");
+  }
+}
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
